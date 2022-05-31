c9812b29f45716761a69955a801f5dcafcdd2a15
vicenterg9 <vicenterg9@gmail.com>
2022-05-31 10:36:14 +0200

    feat: tareaFooter

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { TitleComponent } from './title/title.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    TitleComponent
+    TitleComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[HeaderComponent]
+  exports:[HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
