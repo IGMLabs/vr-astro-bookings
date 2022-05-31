2b8dad97940687a0013ba8e5f03c20355cb7cfc4
vicenterg9 <vicenterg9@gmail.com>
2022-05-31 10:14:03 +0200

    feat: headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Astro Bookings';
+  public title = 'Astro Bookings';
   public  subtitle = "Welcome on board";
   public author = "Vicente Rodriguez";
   public authorURL = "https://www.google.com";
