adde85fac6d2f36dba328b4297fa0d1a53813151
vicenterg9 <vicenterg9@gmail.com>
2022-05-31 11:06:42 +0200

    feat: tarea Reloading

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePage } from './home.page';
+import { ReloadingComponent } from '../shared/reloading/reloading.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +11,7 @@ import { HomePage } from './home.page';
     HomePage
   ],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ],
   exports: [
     HomePage
