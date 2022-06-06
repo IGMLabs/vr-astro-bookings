0f414ab8c05a5c4cbac86aafaef948cbcb8ec59f
vicenterg9 <vicenterg9@gmail.com>
2022-06-06 11:02:50 +0200

    feat: validation forms

--- src/app/contact/contact.form.ts
+++ src/app/contact/contact.form.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 interface Contact{
   name: string;
@@ -18,12 +18,47 @@ export class ContactForm implements OnInit {
 
   constructor(formBuilder:FormBuilder) {
     this.form=formBuilder.group({
-      name:['',Validators.required],
+      name:new FormControl('',[Validators.required, Validators.minLength(2)]),
+      email:new FormControl('',[Validators.required, Validators.email]),
+      message:new FormControl('',[Validators.required, Validators.minLength(2), Validators.maxLength(50)])
     })
    }
 
   ngOnInit(): void {
   }
+
+  public mustShowMessage (controlName :string) : boolean {
+    const control = this.getControl(controlName);
+    if(!control) return false
+    return control.invalid && control.touched;
+  }
+
+  public hasError(ControlName :string): boolean{
+    const control = this.getControl('name');
+
+    if(!control) return false
+    return control.invalid;
+
+  }
+
+  public getErrorMessage(controlName: string) :string {
+    const control = this.getControl(controlName);
+    if (!control) return '';
+    if (!control.errors) return '';
+    const errors = control.errors;
+    let errorMessage = '';
+    errorMessage += errors['required'] ? '🔥 Field is required' : '';
+    errorMessage += errors['email'] ? '🔥 Should be an email' : '';
+    errorMessage += errors['minlength'] ? `🔥 More than ${errors["minlength"].requiredLength} chars` : '';
+    errorMessage += errors['maxlength'] ? `🔥 Less than ${errors["maxlength"].requiredLength} chars` : '';
+    return errorMessage;
+
+  }
+
+  public getControl(ControlName :string): AbstractControl | null{
+    return this.form.get(ControlName);
+  }
+
   public onSave(){
     const  contact = this.form.value;
     console.warn('Send contact message', contact);
