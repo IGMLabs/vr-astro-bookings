6739128a1b030d594409c76fef82eeaaffe4f055
vicenterg9 <vicenterg9@gmail.com>
2022-06-06 09:36:13 +0200

    feat: antes de empezar guardar

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
-import { ContactComponent } from './contact.component';
 import { ContactForm } from './contact.form';
 
 
 @NgModule({
   declarations: [
-    ContactComponent,
     ContactForm
   ],
   imports: [
