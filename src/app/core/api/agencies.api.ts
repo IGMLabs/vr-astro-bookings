24c95741f7857f8aa116d5ff43a3dae87383721c
vicenterg9 <vicenterg9@gmail.com>
2022-06-10 10:21:51 +0200

    feat: enviroment

--- src/app/core/api/agencies.api.ts
+++ src/app/core/api/agencies.api.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Agency } from "./agency.interface";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,8 @@ import { Observable } from "rxjs";
 export class AgenciesApi {
 
   constructor(private http: HttpClient){ }
+
+  private url = environment.apiUrl + 'agencies/';
 
   public getAll$() : Observable<Agency[]>{
     return this.http.get<Agency[]>("http://localhost:3000/agencies");
