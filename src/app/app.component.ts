16d42a37ed5ce42ec90509c4a11e758a0dfcf6cb
vicenterg9 <vicenterg9@gmail.com>
2022-06-17 13:20:36 +0200

    feat: pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'Astro Bookings';
-  public  subtitle = "Welcome on board";
-  public author = "Vicente Rodriguez";
-  public authorURL = "https://www.google.com";
+  // public title = 'Astro Bookings';
+  // public  subtitle = "Welcome on board";
+  // public author = "Vicente Rodriguez";
+  // public authorURL = "https://www.google.com";
+
+  public newVersion = '';
+
+
+  constructor(swUpdate : SwUpdate){
+    swUpdate.versionUpdates.subscribe((event:VersionEvent) => {
+      if (event.type ==='VERSION_READY'){
+        this.newVersion = event.latestVersion.appData ?
+                        JSON.stringify(event.latestVersion.appData)
+                        : event.latestVersion.hash;
+      }
+    });
+
+    swUpdate.checkForUpdate();
+    interval(1000*60).subscribe( () => swUpdate.checkForUpdate() );
+
+  }
+
+
+  public onReload(){
+    window.location.reload();
+  }
+
 }
